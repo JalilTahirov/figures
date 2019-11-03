62035336750327d83a4fcc1d6a6f677900a6fe5b
Jalil Tahirov <jalil.tahirov@outlook.com>
2019-11-03 14:57:54 +0400

    2 not filled empty perfect square

--- index.js
+++ index.js
@@ -13,4 +13,21 @@ const printFilledSquare = size => {
   
   printFilledSquare(4);
   
+  // 2 not filled empty perfect square
+
+const printNotFilledSquare = size => {    
+    let str = '';    
+    for(let j = 0; j < size; j++){
+        for(let i = 0; i < size; i++){
+          if(j === 0||j === size-1||i === 0||i === size-1){
+            str += '#';
+          } else {
+            str += ' ';
+          }            
+        }
+        str += '\n';      
+    }    
+    console.log(str);
+  }  
   
+  printNotFilledSquare(7);
